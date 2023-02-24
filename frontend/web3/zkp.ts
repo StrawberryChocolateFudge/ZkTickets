7e506551fd99783c0e2e1baf5b3ac7cb074c6c2b
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-02-24 18:58:08 +0000

    implemented zkp for the front end

---
+++ frontend/web3/zkp.ts
@@ -0,0 +1,30 @@
+import { CryptoNote, cryptoNote, packToSolidityProof, parseNote, Proof, SolidityProof, generateTicketProof, SnarkArtifacts } from "../../lib/crypto";
+
+export type ParsedNote = { cryptoNote: CryptoNote, netId: number }
+
+export type CryptoNoteDetails = [ParsedNote, string];
+
+export async function getNote(hexNetId): Promise<CryptoNoteDetails> {
+    const netId = parseInt(hexNetId);
+    const noteString = await cryptoNote(netId);
+    const parsedNote = await parseNote(noteString);
+    return [parsedNote, noteString];
+}
+
+export function convertProof(proof: Proof): SolidityProof {
+    return packToSolidityProof(proof);
+}
+
+export async function parseCryptoNote(note: string): Promise<ParsedNote> {
+    return await parseNote(note);
+}
+
+export async function generateProof(cryptoNote: CryptoNote) {
+    const snarkArtifacts: SnarkArtifacts = {
+        wasmFilePath: window.location.origin + "/ticket.wasm",
+        zkeyFilePath: window.location.origin + "/ticket_0001.zkey"
+    }
+    const { proof, publicSignals } = await generateTicketProof({ cryptoNote, snarkArtifacts });
+
+    return convertProof(proof);
+}
