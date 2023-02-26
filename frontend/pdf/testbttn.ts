8436a4dd011f3dc99c2d86d6771c3c6aee1948bd
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-02-26 12:18:56 +0000

    This is for testing purposes

---
+++ frontend/pdf/testbttn.ts
@@ -0,0 +1,10 @@
+// Used only for testing with a testpdf button element
+
+// const testpdf = document.getElementById("testpdf") as HTMLButtonElement;
+
+// testpdf.onclick = async function () {
+//     const noteDetails = await getNote("0xfa2");
+//     const d = noteDetails[1];
+//     const dataUrl = await createQR(d) as string;
+//     await downloadPDF("MY EVENT TITLE HEFKAF IS LONG LIKE THIS", "0.0001", "FTM", dataUrl, d, window.location.href)
+// }
