8afcbb4b80d5d51bd7a4e9baa7d90cf3e4f02740
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-03-17 02:37:42 +0000

    update: getTicketedEvent using wallet extension

--- frontend/tickets.ts
+++ frontend/tickets.ts
@@ -1,9 +1,14 @@
 import { toNoteHex } from "../lib/crypto";
 import { downloadPDF } from "./pdf";
 import { createQR } from "./qrcode";
-import { handleError, appURL, getEventIndex } from "./utils";
-import { formatEther, getContract, getJsonProviderTicketedEvent, getNetworkFromSubdomain, getTicketedEvents, getWeb3Provider, onboardOrSwitchNetwork, purchaseTicket, ZEROADDRESS } from "./web3";
+import { handleError, appURL, getEventIndex, createNewImgElement, createNewTooltipText, appendTooltip } from "./utils";
+import { BTTTESTNETID, FANTOMTESTNETID, formatEther, getContract, getJsonProviderTicketedEvent, getNetworkFromSubdomain, getTicketedEvents, getWeb3Provider, onboardOrSwitchNetwork, purchaseTicket, ZEROADDRESS } from "./web3";
 import { getNote } from "./web3/zkp";
+
+const [CONTRACTADDRESS, NETID, RPCURL] = getNetworkFromSubdomain();
+
+const currencyPriceRow = document.getElementById("currencyPriceRow") as HTMLDivElement;
+
 
 (
     async () => {
@@ -13,6 +18,16 @@ import { getNote } from "./web3/zkp";
         await tsParticles.load("tsparticles", {
             preset: "bigCircles", // also "big-circles" is accepted
         });
+
+        if (NETID === BTTTESTNETID) {
+            const imgEl = createNewImgElement("./bttLogo.svg");
+            appendTooltip(currencyPriceRow, imgEl, null);
+        } else if (NETID === FANTOMTESTNETID) {
+            const imgEl = createNewImgElement("./fantomLogo.svg");
+            appendTooltip(currencyPriceRow, imgEl, null);
+        }
+
+
     })();
 
 const purchaseTicketsSelectorButton = document.getElementById("purchaseTicketsSelectorButton") as HTMLElement;
@@ -28,10 +43,9 @@ const downloadButton = document.getElementById("downloadButton") as HTMLButtonEl
 const eventName = document.getElementById("eventName") as HTMLElement;
 const eventPrice = document.getElementById("eventPrice") as HTMLElement;
 const ticketsLeft = document.getElementById("ticketsLeft") as HTMLElement;
-
+const eventCreator = document.getElementById("eventCreator") as HTMLElement;
 
 const getHandleTicketURL = (index: string) => appURL + `/handleTicket.html?i=${index}`
-
 
 handleTicketsButton.onclick = function () {
     const index = getEventIndex();
@@ -42,31 +56,50 @@ handleTicketsButton.onclick = function () {
 }
 
 purchaseTicketsSelectorButton.onclick = async function () {
-    const index = getEventIndex();
-    if (!index) {
-        return;
-    }
+    const switched = await onboardOrSwitchNetwork(handleError);
 
-    welcomeMessage.classList.add("hide");
-    loadingBanner.classList.remove("hide");
-    purchaseTicketsSelectorButton.classList.add("hide");
-    handleTicketsButton?.classList.add("hide");
+    if (switched) {
 
-    const ticketedEvent = await getJsonProviderTicketedEvent(index, handleError);
-
-    eventName.textContent = ticketedEvent.eventName;
-    eventPrice.textContent = formatEther(ticketedEvent.price);
-    ticketsLeft.textContent = ticketedEvent.availableTickets;
-
-    setTimeout(() => {
-        const eventContainer = document.getElementById("eventContainer") as HTMLElement;
-        eventContainer.classList.remove("hide");
-        welcomeMessage.classList.add("hide");
-        loadingBanner.classList.add("hide");
-        if (ticketedEvent.creator === ZEROADDRESS) {
-            purchaseTicketActionContainer.classList.add("hide")
+        const index = getEventIndex();
+        if (!index) {
+            return;
         }
-    }, 1000);
+
+        welcomeMessage.classList.add("hide");
+        loadingBanner.classList.remove("hide");
+        purchaseTicketsSelectorButton.classList.add("hide");
+        handleTicketsButton?.classList.add("hide");
+
+        const provider = getWeb3Provider();
+        const [CONTRACTADDRESS, NETID, RPCURL] = getNetworkFromSubdomain();
+
+        const contract = await getContract(provider, CONTRACTADDRESS, "ZKTickets.json").catch(err => {
+            handleError("Network error");
+        })
+
+        const ticketedEvent = await getTicketedEvents(contract, index).catch(err => {
+            handleError("Unable to find the event!");
+        })
+
+        if (!ticketedEvent) {
+            handleError("Unable to connect to wallet!")
+        }
+
+        eventCreator.textContent = "Pay To: "+ticketedEvent.creator;
+        eventName.textContent = ticketedEvent.eventName;
+        eventPrice.textContent = formatEther(ticketedEvent.price);
+        ticketsLeft.textContent = ticketedEvent.availableTickets;
+
+        setTimeout(() => {
+            const eventContainer = document.getElementById("eventContainer") as HTMLElement;
+            eventContainer.classList.remove("hide");
+            welcomeMessage.classList.add("hide");
+            loadingBanner.classList.add("hide");
+            if (ticketedEvent.creator === ZEROADDRESS) {
+                purchaseTicketActionContainer.classList.add("hide")
+            }
+        }, 1000);
+    }
 };
 
 purchaseTicketAction.onclick = async function () {
