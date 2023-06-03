c4c2b08945051422a656cc6469513140e5f248db
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-06-03 04:25:54 +0200

    removed unused files

--- copyToDist.sh
+++ copyToDist.sh
@@ -1,7 +1,4 @@
 cp abis/ZKTickets.json dist/ZKTickets.json
-cp abis/ProStaking.json dist/ProStaking.json
-cp abis/TicketPro.json dist/TicketPro.json
-cp abis/EventWarnings.json dist/EventWarnings.json
 
 cp circuits/Ticket/ticket_0001.zkey dist/ticket_final.zkey
 cp circuits/Ticket/ticket_js/ticket.wasm dist/ticket.wasm
@@ -9,4 +6,3 @@ cp circuits/Ticket/ticket_js/ticket.wasm dist/ticket.wasm
 cp frontend/images/bttLogo.svg dist/bttLogo.svg
 cp frontend/images/fantomLogo.svg dist/fantomLogo.svg
 cp frontend/images/tron-trx-logo.svg dist/tron-trx-logo.svg
-cp apk/ZkTickets_Scanner_0.0.1_release.apk dist/ZkTickets_Scanner_0.0.1_release.apk
