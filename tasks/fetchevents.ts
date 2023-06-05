13a0d0f7ddb1538bfba18d228b000de8fd9ec11c
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-06-05 13:01:53 +0200

    updated

--- tasks/fetchevents.ts
+++ tasks/fetchevents.ts
@@ -1,13 +1,13 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { ZKTickets } from "../typechain";
 import { task } from "hardhat/config";
 const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
     const ZkticketsFactory = await hre.ethers.getContractFactory("ZKTickets");
-    const contract = ZkticketsFactory.attach("0x04ce7D262c474A2d55589dCE8DCe23A9678c35E3");
+    const contract = ZkticketsFactory.attach("0x026E01a71C9F0d40a67BbC898e7715424c0cf405") as ZKTickets;
 
     const filter = contract.filters.NewTicketedEventCreated("0xFefE84360b11563cf336E327f1181A97d26DFFe5");
     const res = await contract.queryFilter(filter);
 
-    console.log(res);
 
 }
 
